1ef9eb535c66a1b27e5db7a264c1b4d1eab76db1
jiangwang11 <458793199@qq.com>
2025-09-08 09:01:58 +0800

    Update MapConfigs.js

--- js/MapConfigs.js
+++ js/MapConfigs.js
@@ -102,39 +102,131 @@ window.OverworldMaps = {
   // 通往阳台的门 (12..14,14)
   ...utils.portalLine(12, 14, 14, "Balcony", 19, 23, "up"),
   // 当玩家走到坐标(15, 25)时显示图片
-  [utils.asGridCoord(15, 25)]: [
-        {
-          events: [
-            { type: "textMessage", text: "你发现了一张隐藏的图片！" },
-            { type: "setVisibility", objectId: "npc4", visible: true }
-          ]
-        }
+  // 面向(15,25)时按空格触发
+  [utils.asGridCoord(17, 26)]: [
+    {
+      events: [
+        { type: "textMessage", text: "【新线索】:一个捆的很紧的麻袋" },
+        { type: "showImage", src: "./image in the game/character/2walking.png" },
+        { type: "textMessage", text: "zq警官:是榔头男作案时的风衣帽子和墨镜，看来这的确就是榔头男的家了" },
       ],
+      trigger: "action" // 新增自定义标记，便于后续扩展
+    }
+  ],
   // 当玩家走到坐标(15, 25)时显示图片，
-    [utils.asGridCoord(20, 32)]: [
+    [utils.asGridCoord(19, 32)]: [
         {
           events: [
-            { type: "textMessage", text: "是送货员留下的快递箱！" },
-            { type: "showImage", src: "./image in the game/character/2walking.png" }
+            { type: "textMessage", text: "【新线索】：这应该是快递员送来的包裹，有几处可能是运输时的碰撞导致的凹陷。箱子有一点重量"},
+            { type: "showImage", src: "./image in the game/character/2walking.png" },
+            { type: "textMessage", text: "zq警官:！！扳手！这大小……确实很像凶器！完全有可能造成死者头部的伤痕！"},
+            { type: "textMessage", text: "zq警官:但是这也太干净了。不像是凶器……不过也有可能被凶手细致的清洗过了"},
+            { type: "textMessage", text: "wx警官:很有可能是取货员用这个扳手杀了人，清洗干净再藏到这个快递箱中，嫁祸给快递员！！"},
+            { type: "textMessage", text: "zq警官:大胆假设，小心求证。你有证据吗？"},
           ]
         }
       ],
-  [utils.asGridCoord(29, 41)]: [
+  [utils.asGridCoord(28, 41)]: [
         {
           events: [
-            { type: "textMessage", text: "鞋柜似乎有些异常" },
+            { type: "textMessage", text: "【新线索】：一个简陋的木质鞋柜，看起来有些年头了" },
             { type: "showImage", src: "./image in the game/character/shadow.png" }
           ]
         }
       ],//鞋柜
-  [utils.asGridCoord(40, 28)]: [
+   [utils.asGridCoord(41, 28)]: [
         {
           events: [
-            { type: "textMessage", text: "你发现了一张隐藏的图片！" },
-            { type: "setVisibility", objectId: "npc7", visible: true }
+            { type: "textMessage", text: "【新线索】：很脏很乱的垃圾桶，像是很久没人打理了" },
+            { type: "showImage", src: "./image in the game/character/shadow.png" },
+            { type: "textMessage", text: "zq警官:这榔头男也太不讲卫生了，怎么会这么乱啊" },
           ]
         }
       ],//垃圾桶
+    [utils.asGridCoord(13, 32)]: [
+        {
+          events: [
+            { type: "textMessage", text: "【新线索】：披萨和收据" },
+            { type: "showImage", src: "./image in the game/character/shadow.png" },
+            { type: "textMessage", text: "wx警官:这张单据是披萨外卖员留下的，上面有他的联系方式以及店铺的联系方式。价格和送到的披萨口味一致。" },
+            { type: "textMessage", text: "zq警官:奇怪？？送货员和机车女的收据怎么不见了呢？" },
+            { type: "interactionMenu",
+              title: "披萨和收据",
+              options: [
+            {
+              label: "打开披萨盒",
+              description: "检查披萨盒内部",
+              handler: () => {
+                // 触发冰箱内部检查的文本消息
+                const message = new TextMessage({
+                  text: "你打开了披萨盒，披萨完好无损，似乎确实没有人动过他",
+                  backgroundImage: "./fridge_open.png",
+                  onComplete: () => {
+                    console.log("披萨盒检查完成");
+                  }
+                });
+                message.init(document.querySelector(".game-container"));
+              }
+            },
+            {
+              label: "离开",
+              description: "让我再看看别处",
+              handler: () => {
+                console.log("离开披萨盒");
+              }
+            }
+          ]},
+          ]
+        }
+      ],
+      [utils.asGridCoord(9, 34)]: [
+        {
+          events: [
+            { type: "textMessage", text: "【新线索】：一个置物箱，杂七杂八的东西都堆在一块" },
+            { type: "showImage", src: "./image in the game/character/shadow.png" },
+            { type: "textMessage", text: "wx警官:这都是些啥啊，好乱啊" },
+          ]
+        }
+      ],
+      [utils.asGridCoord(20, 36)]: [
+        {
+          events: [
+            { type: "textMessage", text: "【新线索】：一张款式老旧的布艺沙发靠墙摆放。沙发套看起来有些日子没洗了，颜色暗淡。" },
+            { type: "showImage", src: "./image in the game/character/shadow.png" },
+          ]
+        }
+      ],
+      [utils.asGridCoord(36, 36)]: [
+        {
+          events: [
+            { type: "textMessage", text: "【新线索】：墙边堆着两个扁平的棕色瓦楞纸箱" },
+            { type: "showImage", src: "./image in the game/character/shadow.png" },
+            { type: "textMessage", text: "wx警官:看来这榔头男随时准备搬家逃跑啊，辛亏我们接到线报就一直在这监视" },
+          ]
+        }
+      ],
+      [utils.asGridCoord(26, 15)]: [
+        {
+          events: [
+            { type: "textMessage", text: "【新线索】：破旧的大时钟，看来指针已经停摆好久了" },
+            { type: "showImage", src: "./image in the game/character/shadow.png" },
+          ]
+        }
+      ],
+      [utils.asGridCoord(39, 17)]: [
+        {
+          events: [
+            { type: "textMessage", text: "一盆普通的仙人掌" },
+          ]
+        }
+      ],
+      [utils.asGridCoord(47, 17)]: [
+        {
+          events: [
+            { type: "textMessage", text: "不算名贵的绿植" },
+          ]
+        }
+      ],
     }
   },
   Bedroom: {
