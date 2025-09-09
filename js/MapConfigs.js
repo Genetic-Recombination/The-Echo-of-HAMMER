17b7aa8a29a63b2290c591e91ba3cc53709d27c6
naughtyrabbit12 <3462362907>
2025-09-09 19:57:34 +0800

    OK

--- js/MapConfigs.js
+++ js/MapConfigs.js
@@ -271,8 +271,6 @@ window.OverworldMaps = {
 
       { type: "textMessage", text: "(两位警官都沉默了，目光从尸体移到散落的药片，再移到死者过于整齐的穿着上。现场看起来像是一场…)" },
       { type: "textMessage", text: "wx警官：\n（语气带着不确定的推测）\n“难道…是畏罪自杀？他穿好衣服戴上帽子…是原本想逃跑，但发现逃不掉后，就选择了自我了断？”" , who: "wx"},
-
-      { type: "textMessage", text: "(凝重的背景音乐响起...)" },
       { type: "showImage", src: "./image in the game/article/big_dead.png" },
 
       {
